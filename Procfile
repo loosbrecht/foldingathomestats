7b26292176960bdd8ea26f14960af66002687686
loosbrecht <loos.brecht@gmail.com>
2021-02-11 20:38:17 +0100

    Worker experiment

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: java -jar target/foldingathomestats-1.0-SNAPSHOT.jar $PORT
+worker: sh target/bin/worker
---
+++ src/main/java/com/loos/fahstats/Worker.java
@@ -0,0 +1,8 @@
+package com.loos.fahstats;
+
+public class Worker {
+
+    public static void main(String[] args) {
+        System.out.println("Hellooo");
+    }
+}
